fc98e6f17bbb53dfbbaddf29ba43a0756194b403
taylor kraft <taylorkraft6@gmail.com>
2021-11-04 19:52:25 -0600

    short greeting defined

--- index.js
+++ index.js
@@ -3,3 +3,5 @@ const currentUser = 'Esco'
 const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`
 
 const excitedWelcomeMessage = welcomeMessage.toUpperCase()
+
+const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`
